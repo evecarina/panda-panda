e98d0422bf297e552ab248b934c2f0cc002d873f
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-08-11 19:23:59 -0500

    colocando botones a las imagenes de los panditas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
-var x=document.createElement("span");
+var pictures = ["assets/img/a1.jpg",
+                "assets/img/a2.jpg",
+                "assets/img/a3.jpg",
+                "assets/img/a4.jpg"];
 
-x.textContent = "x";
-var img1=document.getElementById("img1");
-img1.appendChild(x);
+//var section = document.getElementsByTagName("section1");
+
+var div_imagenes = document.getElementById('imagenes');
+for (var i = 0; i < pictures.length; i++) {
+  var div_principal=document.createElement("div");
+div_principal.setAttribute("class","div_principal");
+  var photo = document.createElement('img');
+  photo.setAttribute('src', pictures[i]);
+  var btn_x=document.createElement("button");
+  btn_x.setAttribute("class","btn_x");
+  btn_x.textContent="x";
+  div_imagenes.appendChild(div_principal);
+  div_principal.appendChild(photo);
+  div_principal.appendChild(btn_x);
+
+ 
+
+
+}
+
+
+
